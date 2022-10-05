442daad8a948bd5e154c5811dd276e5c39d2cbf1
Manyo <agusmangini@hotmail.com>
2022-10-05 17:49:43 -0300

    Implemento jugada croupier

--- src/app/juego/mesa/mesa.component.ts
+++ src/app/juego/mesa/mesa.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Carta } from 'src/app/models/carta';
 import { CartaService } from 'src/app/services/carta.service';
 
@@ -7,16 +7,21 @@ import { CartaService } from 'src/app/services/carta.service';
   templateUrl: './mesa.component.html',
   styleUrls: ['./mesa.component.css']
 })
+
 export class MesaComponent implements OnInit {
   jugadaJugador : Carta [] = [];
   jugadaCroupier : Carta [] = [];
   puntajeJugador: number = 0;
   puntajeCroupier: number = 0;
   empezo : boolean = false;
-  perdio : boolean = false;
+  perdioJugador : boolean = false;
   terminoJugada : boolean = false;
+  turnoCroupier : boolean = false;
+  terminoCroupier : boolean = false;
+  perdioCroupier : boolean = false;
   constructor(private cartaService: CartaService) { }
 
+  
   ngOnInit(): void {
   }
 
@@ -24,7 +29,7 @@ export class MesaComponent implements OnInit {
     this.puntajeJugador = this.calcularPuntaje(this.jugadaJugador);
     this.puntajeCroupier = this.calcularPuntaje(this.jugadaCroupier);
   }
-  
+
   private agregarCarta(jugada : Carta[]): void {
     jugada.push(this.cartaService.obtenerCartaAleatoria());
   }
@@ -56,17 +61,34 @@ export class MesaComponent implements OnInit {
     if(this.calcularPuntaje(jugada)> 21){
       for (let carta of jugada) {
         if (carta.valorNumerico === 11){
-          carta.valorNumerico = 1;
-          this.terminoJugada = this.calcularPuntaje(jugada) === 21;  //En caso de que la ultima carta sea un as teniendo un puntaje de 20
-          return;
+            carta.valorNumerico = 1;
+            this.terminoJugada = this.calcularPuntaje(jugada) === 21;  //En caso de que la ultima carta sea un as teniendo un puntaje de 20
+            return;
+          }    
         }
+        this.perdioJugador = true;
+        this.terminoJugada = true;
+      } else if (this.calcularPuntaje(jugada) === 21) {
+        this.terminoJugada = true;
       }
-      this.perdio = true;
-      this.terminoJugada = true;
-    } else if (this.calcularPuntaje(jugada) === 21) {
-      this.terminoJugada = true;
-    }
   }
+
+  private chequearCroupier(jugada: Carta[]): void{
+    if(this.calcularPuntaje(jugada)> 21){
+      for (let carta of jugada) {
+        if (carta.valorNumerico === 11){
+            carta.valorNumerico = 1;
+            this.terminoCroupier = this.calcularPuntaje(jugada) > 16;
+            return;
+          }    
+      }
+        this.perdioCroupier = true;
+        this.terminoCroupier = true;
+      } else if (this.calcularPuntaje(jugada) > 16) {
+        this.terminoCroupier = true;
+      }
+    }
+  
 
   pedirCarta() : void{
     this.agregarCarta(this.jugadaJugador);
@@ -84,11 +106,30 @@ export class MesaComponent implements OnInit {
     this.puntajeJugador = 0;
     this.puntajeCroupier = 0;
     this.empezo = false;
-    this.perdio = false;
+    this.perdioJugador = false;
     this.terminoJugada = false;
+    this.turnoCroupier = false;
+    this.terminoCroupier = false;
+    this.perdioCroupier = false;
+  }
+
+  private chequearGanador(): void{
+  }
+
+  private jugarCroupier(){
+    this.turnoCroupier = true;
+    this.jugadaCroupier.shift();
+
+    while(!this.terminoCroupier){
+      this.agregarCarta(this.jugadaCroupier);
+      this.chequearCroupier(this.jugadaCroupier);
+      this.actualizarPuntajes();
+    }
+
+    this.chequearGanador();
   }
 
   continuar() : void {
-
+    this.jugarCroupier();
   }
 }
