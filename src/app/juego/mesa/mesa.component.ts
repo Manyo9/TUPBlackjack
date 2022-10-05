44536dfef2ca72a0dfc7f4ffa5639f9896b2619e
Manyo <agusmangini@hotmail.com>
2022-10-05 19:29:27 -0300

    Implemento lógica en clase Jugador

--- src/app/juego/mesa/mesa.component.ts
+++ src/app/juego/mesa/mesa.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Carta } from 'src/app/models/carta';
+import { Jugador } from 'src/app/models/jugador';
 import { CartaService } from 'src/app/services/carta.service';
 
 @Component({
@@ -9,170 +10,102 @@ import { CartaService } from 'src/app/services/carta.service';
 })
 
 export class MesaComponent implements OnInit {
-  jugadaJugador : Carta [] = [];
-  jugadaCroupier : Carta [] = [];
-  puntajeJugador: number = 0;
-  puntajeCroupier: number = 0;
-  empezo : boolean = false;
-  perdioJugador : boolean = false;
-  terminoJugada : boolean = false;
-  turnoCroupier : boolean = false;
-  terminoCroupier : boolean = false;
-  perdioCroupier : boolean = false;
-  terminoJuego : boolean = false;
-  mensajeFinal : string;
+  jugador: Jugador;
+  croupier: Jugador;
+  empezo: boolean = false;
+  turnoCroupier: boolean = false;
+  terminoJuego: boolean = false;
+  mensajeFinal: string;
 
   constructor(private cartaService: CartaService) { }
 
-  
+
   ngOnInit(): void {
+    this.reiniciar();
   }
 
-  private actualizarPuntajes(): void {
-    this.puntajeJugador = this.calcularPuntaje(this.jugadaJugador);
-    this.puntajeCroupier = this.calcularPuntaje(this.jugadaCroupier);
+  private obtenerCarta(): Carta {
+    return this.cartaService.obtenerCartaAleatoria();
   }
 
-  private agregarCarta(jugada : Carta[]): void {
-    jugada.push(this.cartaService.obtenerCartaAleatoria());
+  private obtenerCartaDadaVuelta(): Carta {
+    return this.cartaService.obtenerDadaVuelta();
   }
 
-  private agregarCartaDadaVuelta(jugada : Carta[]): void {
-    jugada.push(this.cartaService.obtenerDadaVuelta());
+  repartir(): void {
+    this.jugador.tomarCarta(this.obtenerCarta());
+    this.jugador.tomarCarta(this.obtenerCarta());
+    this.croupier.tomarCarta(this.obtenerCartaDadaVuelta());
+    this.croupier.tomarCarta(this.obtenerCarta());
+    this.empezo = true;
   }
 
-  repartir() : void {
-    this.agregarCarta(this.jugadaJugador);
-    this.agregarCarta(this.jugadaJugador);
-    this.agregarCartaDadaVuelta(this.jugadaCroupier);
-    this.agregarCarta(this.jugadaCroupier);
-    this.empezo=true;
-    this.chequearPuntos(this.jugadaJugador);
-    this.actualizarPuntajes();
+  pedirCarta(): void {
+    this.jugador.tomarCarta(this.obtenerCarta());
   }
 
-  private calcularPuntaje(jugada: Carta[]): number{
-    let acumulador: number = 0;
-    jugada.forEach(carta => {
-      acumulador += carta.valorNumerico; 
-    });
-    return acumulador;
+  terminarJugada(): void {
+    this.jugador.terminoJugada = true;
   }
 
-  private chequearPuntos(jugada: Carta[]): void {
-
-    if(this.calcularPuntaje(jugada)> 21){
-      for (let carta of jugada) {
-        if (carta.valorNumerico === 11){
-            carta.valorNumerico = 1;
-            this.terminoJugada = this.calcularPuntaje(jugada) === 21;  //En caso de que la ultima carta sea un as teniendo un puntaje de 20
-            return;
-          }    
-        }
-        this.perdioJugador = true;
-        this.terminoJugada = true;
-      } else if (this.calcularPuntaje(jugada) === 21) {
-        this.terminoJugada = true;
-      }
-  }
-
-  private chequearCroupier(jugada: Carta[]): void{
-    if(this.calcularPuntaje(jugada)> 21){
-      for (let carta of jugada) {
-        if (carta.valorNumerico === 11){
-            carta.valorNumerico = 1;
-            this.terminoCroupier = this.calcularPuntaje(jugada) > 16;
-            return;
-          }    
-      }
-        this.perdioCroupier = true;
-        this.terminoCroupier = true;
-      } else if (this.calcularPuntaje(jugada) > 16) {
-        this.terminoCroupier = true;
-      }
-    }
-  
-
-  pedirCarta() : void{
-    this.agregarCarta(this.jugadaJugador);
-    this.chequearPuntos(this.jugadaJugador);
-    this.actualizarPuntajes();
-  }
-
-  terminarJugada() : void {
-    this.terminoJugada = true;
-  }
-
-  reiniciar() : void {
-    this.jugadaJugador = [];
-    this.jugadaCroupier = [];
-    this.puntajeJugador = 0;
-    this.puntajeCroupier = 0;
+  reiniciar(): void {
     this.empezo = false;
-    this.perdioJugador = false;
-    this.terminoJugada = false;
     this.turnoCroupier = false;
-    this.terminoCroupier = false;
-    this.perdioCroupier = false;
     this.terminoJuego = false;
+    this.jugador = new Jugador('Jugador', [], 0, false, false, false);
+    this.croupier = new Jugador('Croupier', [], 0, true, false, false);
   }
 
-  private chequearGanador(): void{
-    if(this.perdioCroupier && !this.perdioJugador){
-      //gano jugador
+  private chequearGanador(): void {
+    if (!this.jugador.perdio && this.croupier.perdio) {
+      //gano jugador porque se pasó el croupier
       this.mensajeFinal = "Ganaste! Felicidades!";
-    }
-    else if (this.perdioJugador && !this.perdioCroupier){
-      //gano croupier
+
+    } else if (this.jugador.perdio && !this.croupier.perdio) {
+      //gano croupier porque se pasó el jugador
       this.mensajeFinal = "Perdiste! Mejor suerte la próxima!";
-    }
-    else if (!this.perdioJugador && !this.perdioCroupier) {
+
+    } else if (!this.jugador.perdio && !this.croupier.perdio) {
       //ninguno se pasó
-      if (this.puntajeJugador === this.puntajeCroupier) {
+      // chequeo de blackjack
+      if (this.jugador.puntos === this.croupier.puntos) {
         //igual puntaje
-        if(this.puntajeJugador === 21) {
-          if(this.jugadaJugador.length === this.jugadaCroupier.length) {
-            //empate
-            this.mensajeFinal = "Empataron!";
-          }
-          else if(this.jugadaJugador.length > this.jugadaCroupier.length) {
-            //gano croupier
-            this.mensajeFinal = "Perdiste! Mejor suerte la próxima!";
-          }
-          else{
-            //gano jugador
-            this.mensajeFinal = "Ganaste! Felicidades!";
-          }
+        if (this.jugador.tieneBlackjack() && !this.croupier.tieneBlackjack()) {
+          //gana jugador por blackjack
+        } else if (!this.jugador.tieneBlackjack() && this.croupier.tieneBlackjack()) {
+          // gana croupier por blackjack
+        } else {
+          //empate
+          this.mensajeFinal = "Empataron!";
         }
-        //empate
-        this.mensajeFinal = "Empataron!";
-      }
-      else if (this.puntajeJugador > this.puntajeCroupier) {
-        //gana jugador
+      } else if (this.jugador.puntos > this.croupier.puntos) {
+        //gana jugador por mayor puntaje
         this.mensajeFinal = "Ganaste! Felicidades!";
-      }
-      else {
-        //gana croupier
+      } else {
+        //gana croupier por mayor puntaje
         this.mensajeFinal = "Perdiste! Mejor suerte la próxima!";
       }
+
+    } else {
+      // Empate ya que ambos se pasaron
+      this.mensajeFinal = "Empataron!";
     }
+
     this.terminoJuego = true;
   }
 
-  private jugarCroupier(){
+  private jugarCroupier() {
     this.turnoCroupier = true;
-    this.jugadaCroupier.shift();
+    this.croupier.quitarBocaAbajo();
 
-    while(!this.terminoCroupier){
-      this.agregarCarta(this.jugadaCroupier);
-      this.chequearCroupier(this.jugadaCroupier);
-      this.actualizarPuntajes();
+    while (!this.croupier.terminoJugada) {
+      this.croupier.tomarCarta(this.obtenerCarta());
     }
 
     this.chequearGanador();
   }
 
-  continuar() : void {
+  continuar(): void {
     this.jugarCroupier();
   }
 }
---
+++ src/app/models/jugador.ts
@@ -0,0 +1,68 @@
+import { Carta } from "./carta";
+
+export class Jugador {
+    nombre: string;
+    mano: Carta[];
+    puntos: number;
+    esCroupier: boolean;
+    terminoJugada: boolean;
+    perdio: boolean;
+
+    constructor (nombre: string, mano: Carta[], puntos: number, esCroupier: boolean, terminoJugada: boolean, perdio: boolean) {
+        this.nombre = nombre;
+        this.mano = mano;
+        this.puntos = puntos;
+        this.esCroupier = esCroupier;
+        this.terminoJugada = terminoJugada;
+        this.perdio = perdio;
+    }
+
+    tomarCarta(carta: Carta): void{
+        this.mano.push(carta);
+        this.chequearCondicion();
+        this.puntos = this.calcularPuntos();
+    }
+
+    tieneBlackjack(): boolean{
+        return (this.puntos === 21 && this.mano.length === 2)
+    }
+
+    calcularPuntos(): number{
+        let acumulador: number = 0;
+        this.mano.forEach(carta => {
+            acumulador += carta.valorNumerico; 
+        });
+        return acumulador;
+    }
+
+    forzarPlantado() : boolean {
+        if(this.esCroupier){
+            return this.calcularPuntos() > 16;
+        } else {
+            return this.calcularPuntos() === 21;
+        }
+    }
+
+    chequearCondicion(): void {
+        if(this.calcularPuntos() > 21){
+          for (let carta of this.mano) {
+            if (carta.valorNumerico === 11){
+                carta.valorNumerico = 1;
+                this.terminoJugada = this.forzarPlantado();  //Failsafe por si la ultima carta pedida es un as
+                return;
+              }    
+            }
+            this.perdio = true;
+            this.terminoJugada = true;
+        } else if (this.forzarPlantado()) {
+            this.terminoJugada = true;
+        }
+    }
+
+    quitarBocaAbajo(): void {
+        if(this.esCroupier){
+            this.mano.shift();
+        }
+    }
+
+}
