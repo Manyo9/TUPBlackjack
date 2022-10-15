cca494cbf08279175ed65ee16eb6bf11162dee98
Abraguas <fmoises2002@gmail.com>
2022-10-15 19:46:33 -0300

    Implemento partida desde api a mesa (incompleto)

--- src/app/juego/mesa/mesa.component.ts
+++ src/app/juego/mesa/mesa.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Carta } from 'src/app/models/carta';
 import { Jugador } from 'src/app/models/jugador';
+import { Partida } from 'src/app/models/partida';
+import { ResultadoGenerico } from 'src/app/models/resultado-generico';
 import { CartaService } from 'src/app/services/carta.service';
+import { PartidaService } from 'src/app/services/partida.service';
 
 @Component({
   selector: 'app-mesa',
@@ -9,79 +13,119 @@ import { CartaService } from 'src/app/services/carta.service';
   styleUrls: ['./mesa.component.css']
 })
 
-export class MesaComponent implements OnInit {
-  jugador: Jugador;
-  croupier: Jugador;
-  empezo: boolean = false;
-  turnoCroupier: boolean = false;
-  terminoJuego: boolean = false;
+export class MesaComponent implements OnInit, OnDestroy {
+  partida: Partida;
+  // jugador: Jugador;
+  // croupier: Jugador;
+  // empezo: boolean = false;
+  // turnoCroupier: boolean = false;
+  // terminoJuego: boolean = false;
   mensajeFinal: string;
-
-  constructor(private cartaService: CartaService) { }
-
-
-  ngOnInit(): void {
-    this.reiniciar();
+  subscription: Subscription;
+  constructor(private cartaService: CartaService, private partidaService: PartidaService) {
+    this.partida = new Partida();
+    this.subscription = new Subscription();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
-  private obtenerCarta(): Carta {
-    return this.cartaService.obtenerCartaAleatoria();
+  ngOnInit(): void {
+    this.cargarPartida();
+    this.reiniciar();
+  }
+  private cargarPartida(): void {
+    this.subscription.add(
+      this.partidaService.empezarPartida().subscribe({
+        next: (r: ResultadoGenerico) => {
+          if (r.ok) {
+            this.partida = r.resultado as Partida;
+            console.log(this.partida.jugador);
+          }
+          else {
+            console.error(r.mensaje);
+          }
+        },
+        error: (e) => {
+          console.error(e);
+        }
+      })
+    )
+  }
+  private obtenerCarta(): any {
+
+    this.subscription.add(
+      this.cartaService.obtenerCarta(this.partida.idPartida).subscribe({
+        next: (r: ResultadoGenerico) => {
+          if (r.ok) {
+            return r.resultado[0];
+          } else {
+            console.error(r.mensaje);
+            return undefined;
+          }
+        },
+        error: (e) => {
+          console.error(e);
+          return undefined;
+        }
+      })
+    )
+    return undefined;
   }
 
   private obtenerCartaDadaVuelta(): Carta {
     return this.cartaService.obtenerDadaVuelta();
   }
 
-  repartir(): void {
-    this.jugador.tomarCarta(this.obtenerCarta());
-    this.jugador.tomarCarta(this.obtenerCarta());
-    this.croupier.tomarCarta(this.obtenerCartaDadaVuelta());
-    this.croupier.tomarCarta(this.obtenerCarta());
-    this.empezo = true;
+  async repartir() {
+    this.partida.jugador.tomarCarta(this.pedirCarta());
+    this.partida.jugador.tomarCarta(this.pedirCarta());
+    // this.partida.croupier.tomarCarta(this.obtenerCartaDadaVuelta()); 
+    // this.partida.croupier.tomarCarta(this.obtenerCarta()); // debe implementar un metodo distinto al del jugador
   }
 
   pedirCarta(): void {
-    this.jugador.tomarCarta(this.obtenerCarta());
+    this.partida.jugador.tomarCarta(this.obtenerCarta());
   }
 
   terminarJugada(): void {
-    this.jugador.terminoJugada = true;
+    this.partida.jugador.terminoJugada = true;
   }
 
   reiniciar(): void {
-    this.empezo = false;
-    this.turnoCroupier = false;
-    this.terminoJuego = false;
+    this.partida.empezo = false;
+    this.partida.turnoCroupier = false;
+    this.partida.terminoJuego = false;
     this.mensajeFinal = '';
-    this.jugador = new Jugador('Jugador', [], 0, false, false, false);
-    this.croupier = new Jugador('Croupier', [], 0, true, false, false);
+    this.partida.jugador = new Jugador('Jugador', [], 0, false, false, false);
+    this.partida.croupier = new Jugador('Croupier', [], 0, true, false, false);
   }
 
   private chequearGanador(): void {
-    if (!this.jugador.perdio && this.croupier.perdio) {
+    if (!this.partida.jugador.perdio && this.partida.croupier.perdio) {
       //gano jugador porque se pasó el croupier
       this.mensajeFinal = "Ganaste! el croupier se pasó! Felicidades!";
 
-    } else if (this.jugador.perdio && !this.croupier.perdio) {
+    } else if (this.partida.jugador.perdio && !this.partida.croupier.perdio) {
       //gano croupier porque se pasó el jugador
       this.mensajeFinal = "Perdiste! Te pasaste! Mejor suerte la próxima!";
 
-    } else if (!this.jugador.perdio && !this.croupier.perdio) {
+    } else if (!this.partida.jugador.perdio && !this.partida.croupier.perdio) {
       //ninguno se pasó
       // chequeo de blackjack
-      if (this.jugador.puntos === this.croupier.puntos) {
+      if (this.partida.jugador.puntos === this.partida.croupier.puntos) {
         //igual puntaje
-        if (this.jugador.tieneBlackjack() && !this.croupier.tieneBlackjack()) {
+        if (this.partida.jugador.tieneBlackjack() && !this.partida.croupier.tieneBlackjack()) {
           //gana jugador por blackjack
           this.mensajeFinal = "Ganaste por tener blackjack."
-        } else if (!this.jugador.tieneBlackjack() && this.croupier.tieneBlackjack()) {
+        } else if (!this.partida.jugador.tieneBlackjack() && this.partida.croupier.tieneBlackjack()) {
           // gana croupier por blackjack
           this.mensajeFinal = "Perdiste porque el croupier obtuvo blackjack."
         } else {
           //empate
           this.mensajeFinal = "Empataron por igualdad de puntos.";
         }
-      } else if (this.jugador.puntos > this.croupier.puntos) {
+      } else if (this.partida.jugador.puntos > this.partida.croupier.puntos) {
         //gana jugador por mayor puntaje
         this.mensajeFinal = "Ganaste por tener más puntos! Felicidades!";
       } else {
@@ -93,15 +137,15 @@ export class MesaComponent implements OnInit {
       // Empate ya que ambos se pasaron
       this.mensajeFinal = "Empataron! Ambos perdieron!";
     }
-    this.terminoJuego = true;
+    this.partida.terminoJuego = true;
   }
 
   private jugarCroupier() {
-    this.turnoCroupier = true;
-    this.croupier.quitarBocaAbajo();
+    this.partida.turnoCroupier = true;
+    this.partida.croupier.quitarBocaAbajo();
 
-    while (!this.croupier.terminoJugada) {
-      this.croupier.tomarCarta(this.obtenerCarta());
+    while (!this.partida.croupier.terminoJugada) {
+      this.partida.croupier.tomarCarta(this.obtenerCarta());
     }
 
     this.chequearGanador();
